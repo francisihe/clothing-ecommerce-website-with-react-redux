15bfcd5ef73e18b805f20ecf7c4d5f57e2ca5b84
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-04-28 21:55:19 +0100

    ..

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -18,12 +18,15 @@ const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                {cartItems.map(item => 
-                    <CartItem 
-                        key={item.id}
-                        cartItem={item} 
-                    /> 
-                )}
+                {(cartItems.length < 1)
+                    ? <span className='empty-message'>Your cart is empty</span>
+                    : (cartItems.map(item => 
+                        <CartItem 
+                            key={item.id}
+                            cartItem={item} 
+                        /> 
+                    ))
+                }
             </div>
             <Button 
                 buttonType=''
